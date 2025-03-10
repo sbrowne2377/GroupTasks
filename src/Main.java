0a872af406fed4521b930e9fc09106206f8c6dd4
grace-ahn2 <graceahn41@gmail.com>
2025-03-10 18:26:36 -0400

    new repository

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        String fruits[] = {"Apple", "Banana", "Cherry", "Date", "Banana", "Apple"};
+
+        for (int i = 0; i < fruits.length; i++) {
+
+        }
+
+
     }
 }
